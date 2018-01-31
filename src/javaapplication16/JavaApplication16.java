42224aa8ed8b77f87a09c4eabce28212b4880a76
Maite Chirivella <maitechirivella.profesor@gmail.com>
2018-01-31 21:14:26 +0100

    Update JavaApplication16.java
    Añado clase Scanner

--- src/javaapplication16/JavaApplication16.java
+++ src/javaapplication16/JavaApplication16.java
@@ -15,7 +15,7 @@ public class JavaApplication16 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
     }
     
 }
